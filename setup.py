c31bf224bfa51accb1e345612bae75900036e078
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2020-03-22 14:32:26 +0100

    Bump version: 0.4.4 → 0.4.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 short_description = "No description has been added so far."
 
-version = "0.4.4"
+version = "0.4.5"
 
 try:
     if (Path().parent / "README.rst").is_file():
